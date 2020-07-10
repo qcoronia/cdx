e2ea4b5d0ce4d3a4ea96589b081af6312427ffa9
Paul Coronia <pa.coronia@accelerategroup.net>
2020-07-10 11:29:16 +0800

    Initial commit

---
+++ cdx/DirContext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace dirx
+{
+    public class DirContext
+    {
+        public DirContext(string activeDir)
+        {
+            ActiveDir = activeDir;
+            SearchTerm = string.Empty;
+            AltTargetFolders = Directory.GetDirectories(ActiveDir)
+                .Select(e => Path.GetFileName(e))
+                .Where(e => string.IsNullOrEmpty(SearchTerm) ? true : e.StartsWith(SearchTerm))
+                .ToList();
+            TargetFolder = AltTargetFolders.FirstOrDefault();
+        }
+
+        public string ActiveDir { get; set; }
+
+        public string TargetFolder { get; set; }
+
+        public List<string> AltTargetFolders { get; set; }
+
+        public string SearchTerm { get; set; }
+
+        public void MoveBack()
+        {
+            var dirName = Path.GetFileName(ActiveDir);
+            var parent = Directory.GetParent(ActiveDir);
+            if (parent == null)
+            {
+                return;
+            }
+
+            ActiveDir = parent.FullName;
+            TargetFolder = dirName;
+            AltTargetFolders = Directory.GetDirectories(ActiveDir)
+                .Select(e => Path.GetFileName(e))
+                .Where(e => string.IsNullOrEmpty(SearchTerm) || e.StartsWith(SearchTerm))
+                .ToList();
+            SearchTerm = string.Empty;
+        }
+
+        public void MoveForward()
+        {
+            SearchTerm = string.Empty;
+            ActiveDir = Path.Combine(ActiveDir, TargetFolder);
+            AltTargetFolders = Directory.GetDirectories(ActiveDir)
+                .Select(e => Path.GetFileName(e))
+                .Where(e => string.IsNullOrEmpty(SearchTerm) || e.StartsWith(SearchTerm))
+                .ToList();
+            TargetFolder = AltTargetFolders.FirstOrDefault() ?? string.Empty;
+        }
+
+        public void SelectPrev()
+        {
+            var index = AltTargetFolders.IndexOf(TargetFolder);
+            TargetFolder = AltTargetFolders.ElementAt(Math.Clamp(index - 1, 0, AltTargetFolders.Count - 1));
+        }
+
+        public void SelectNext()
+        {
+            var index = AltTargetFolders.IndexOf(TargetFolder);
+            TargetFolder = AltTargetFolders.ElementAt(Math.Min(AltTargetFolders.Count - 1, index + 1));
+        }
+
+        public void SetSearchTerm(string newSearchTerm)
+        {
+            SearchTerm = newSearchTerm;
+            AltTargetFolders = Directory.GetDirectories(ActiveDir)
+                .Select(e => Path.GetFileName(e))
+                .Where(e => string.IsNullOrEmpty(SearchTerm) || e.StartsWith(SearchTerm))
+                .ToList();
+            TargetFolder = AltTargetFolders.FirstOrDefault() ?? string.Empty;
+        }
+    }
+}
---
+++ cdx/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace dirx
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var context = new DirContext(Directory.GetParent(Directory.GetCurrentDirectory()).FullName);
+
+            Render(context, context.ActiveDir);
+
+            var key = Console.ReadKey();
+            while (key.Key != ConsoleKey.Enter)
+            {
+                var prevPrint = Path.Combine(context.ActiveDir, context.TargetFolder);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.Escape:
+                        Environment.Exit(0);
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        context.MoveBack();
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        context.SelectPrev();
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        context.MoveForward();
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        context.SelectNext();
+                        break;
+
+                    case ConsoleKey.Backspace:
+                        if (context.SearchTerm.Length <= 0)
+                        {
+                            context.MoveBack();
+                        }
+                        else
+                        {
+                            context.SetSearchTerm(context.SearchTerm.Substring(0, Math.Max(0, context.SearchTerm.Length - 1)));
+                        }
+                        break;
+
+                    default:
+                        if ("1234567890 abcdefghijklmnopqrstuvwxyz".Contains(key.KeyChar.ToString().ToLower()))
+                        {
+                            context.SetSearchTerm($"{context.SearchTerm}{key.KeyChar}");
+                        }
+                        break;
+                }
+
+                Render(context, prevPrint);
+
+                key = Console.ReadKey(true);
+            }
+
+            Directory.SetCurrentDirectory(context.ActiveDir);
+
+            Console.ResetColor();
+
+            Console.WriteLine($"The current dir is now: '{Directory.GetCurrentDirectory()}'");
+        }
+
+        private static void Render(DirContext context, string prevPrint)
+        {
+            var curPrint = new StringBuilder();
+            Console.CursorLeft = 0;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(context.ActiveDir);
+            curPrint.Append(context.ActiveDir);
+            Console.Write(Path.DirectorySeparatorChar);
+            curPrint.Append(Path.DirectorySeparatorChar);
+
+            if (context.SearchTerm.Length > 0 && string.IsNullOrEmpty(context.TargetFolder))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            Console.Write(context.SearchTerm);
+            curPrint.Append(context.SearchTerm);
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write(context.TargetFolder.Substring(Math.Min(context.TargetFolder.Length, context.SearchTerm.Length)));
+            curPrint.Append(context.TargetFolder.Substring(Math.Min(context.TargetFolder.Length, context.SearchTerm.Length)));
+
+            Console.Write(new string(' ', Math.Max(curPrint.Length, prevPrint.Length)));
+
+            Console.CursorLeft = context.ActiveDir.Length;
+            Console.ResetColor();
+        }
+    }
+}
